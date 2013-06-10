246bf6cccf644d01674c2fd5e25128970e6119dd
Jirka Vejrazka <Jirka.Vejrazka@gmail.com>
2013-06-10 22:23:16 +0200

    added printer-friendly version (requested by users)

--- dna/urls.py
+++ dna/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('med.dna.views',
     url(r'^sample_matching/$', 'match_dna_sample', name='match_dna_sample'),
+    url(r'^sample_matching/print/', 'dna_comparison_for_print',
+                                    name='dna_comparison_for_print'),
 )
--- dna/views.py
+++ dna/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.simple import direct_to_template
+from django.utils.timezone import now
 from forms import DNASampleForm
 from utils import get_best_offset, get_diffs, get_triplets
 
@@ -12,15 +13,39 @@ def match_dna_sample(request):
             ref_name = reference_dna.name 
             dna_sample = dna_sample.strip('\r\n').upper()
             best_offset = get_best_offset(ref_dna, dna_sample)
-            diffs = get_diffs(ref_dna, dna_sample, best_offset,
-                              show_index_numbers=True)
             changes = get_triplets(ref_dna, dna_sample, best_offset,
                                    different_only=True)
-            return direct_to_template(request, 'dna_matching_result.html', 
-                                      extra_context={'reference': ref_name,
-                                                     'diffs': diffs,
+
+            # save data to session for print
+            request.session['timestamp'] = now()
+            request.session['best_offset'] = best_offset
+            request.session['reference_dna'] = reference_dna
+            request.session['changes'] = changes
+            request.session['dna_sample'] = dna_sample
+
+            return direct_to_template(request, 'dna_matching_result.html',
+                                      extra_context={'reference_name': ref_name,
                                                      'changes': changes})
     else:
         form = DNASampleForm()
     return direct_to_template(request, 'dna_sample_form.html', 
                               extra_context={'form': form})
+
+def dna_comparison_for_print(request):
+    try:
+        # pick up data from session
+        timestamp = request.session['timestamp']
+        best_offset = request.session['best_offset']
+        reference_dna = request.session['reference_dna']
+        changes = request.session['changes']
+        dna_sample = request.session['dna_sample']
+    except KeyError:
+        return direct_to_template(request, 'dna_matching_result_for_print.html',
+                              extra_context={'session_error': True})
+    diffs = get_diffs(reference_dna.dna, dna_sample, best_offset,
+                      show_index_numbers=True)
+    return direct_to_template(request, 'dna_matching_result_for_print.html',
+                              extra_context={'reference': reference_dna,
+                                             'changes': changes,
+                                             'diffs': diffs,
+                                             'timestamp': timestamp})
