80408e71dda2eb43c32803468d3462885e74740e
Jirka Vejrazka <Jirka.Vejrazka@gmail.com>
2016-10-04 11:57:31 +0200

    fixed (and improved) glukokortikoidy, now (almost) purely JS-based, no Ajax callbacks

--- glukokortikoidy/forms.py
+++ glukokortikoidy/forms.py
@@ -11,18 +11,20 @@ class GForm(forms.Form):
     cortison = forms.FloatField(min_value=0.0, required=False)
     betamethason = forms.FloatField(min_value=0.0, required=False)
     triamcinolon = forms.FloatField(min_value=0.0, required=False)
-    
-    def clean(self):
-        '''allow only one field'''
-        super(GForm, self).clean()
-        cnt = 0
-        for field_name in self.fields:
-            try:
-                if self.data[field_name]:
-                    cnt += 1
-            except KeyError:
-                pass
-        if cnt != 1:
-            raise ValidationError('Only and only one field must be entered!')
-        return self.cleaned_data
-    
+
+## all logic is not in JavaScript
+#
+#     def clean(self):
+#         '''allow only one field'''
+#         super(GForm, self).clean()
+#         cnt = 0
+#         for field_name in self.fields:
+#             try:
+#                 if self.data[field_name]:
+#                     cnt += 1
+#             except KeyError:
+#                 pass
+#         if cnt != 1:
+#             raise ValidationError('Only and only one field must be entered!')
+#         return self.cleaned_data
+#
--- glukokortikoidy/views.py
+++ glukokortikoidy/views.py
@@ -2,16 +2,8 @@
 
 from django.shortcuts import render
 from .forms import GForm
-from .calculations import calculate_values
 
 def calculator(request):
-    if request.method == 'POST':
-        form = GForm(request.POST)
-        if form.is_valid():
-            recalculated_data = calculate_values(form.cleaned_data)
-            return render(request, 'glukokortikoidy.html',
-                                   {'data': recalculated_data})
-    else:
-        form = GForm()
+    form = GForm()
     return render(request, 'glukokortikoidy/form.html',
                               {'form': form})
